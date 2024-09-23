1aa0f2239deae181a0274704db4cc341a1c0a117
Roudi22 <roudi.alzkemi@gmail.com>
2024-09-23 11:40:35 +0200

    create the server

---
+++ back-end/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { connectDB } from './db.js';
+import cookieParser from 'cookie-parser';
+import router from './routes/auth.js';
+import hallrouter from './routes/hall.js';
+import movierouter from './routes/movie.js';
+import userRouter from './routes/user.js';
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/auth', router);
+app.use('/api/hall', hallrouter);
+app.use('/api/movie', movierouter);
+app.use('/api/user', userRouter);
+app.listen(process.env.PORT || 5001, () => {
+    try {
+        connectDB();
+        console.log('Server started at', process.env.PORT || 5001);
+    } catch (error) {
+        console.error('Server failed to start');
+        process.exit(1);
+    }
+});
