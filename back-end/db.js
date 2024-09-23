9c96a492850cba28df3598c91e60ccee0853e193
Roudi22 <roudi.alzkemi@gmail.com>
2024-09-23 11:40:53 +0200

    function to connect with the database

---
+++ back-end/db.js
@@ -0,0 +1,15 @@
+// Code to connect to MongoDB database
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed');
+        process.exit(1);
+    }
+};
