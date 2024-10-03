ee6dce9efe53dd03e0de4b5c3b58088828d99125
Roudi22 <roudi.alzkemi@gmail.com>
2024-10-03 20:33:46 +0200

    flytta alla funktioner till controllers mapp

--- back-end/routes/hall.js
+++ back-end/routes/hall.js
@@ -1,34 +1,34 @@
 import Hall from "../models/Hall.js";
 import express from "express";
 import Seat from "../models/Seat.js";
+import Showtime from "../models/Showtime.js";
+import Movie from "../models/Movie.js";
+import { createHall, deleteHalls, getHallById, getHalls, getSeatInfo, getSeatsOfHallAtShowtime, getShowtimesOfMovieInHall } from "../controllers/hallController.js";
 const hallrouter = express.Router();
 
 // Create a new hall
-hallrouter.post("/", async (req, res) => {
-  try {
-    const { hallNumber, seatQuantity } = req.body;
-    const hallExists = await Hall.findOne({ hallNumber });
-    if (hallExists) {
-      return res.status(400).json({ error: "Hall already exists" });
-    }
-    if (seatQuantity > 40) {
-      return res.status(400).json({ error: "Seat quantity exceeds limit" });
-    }
-    if (!hallNumber || !seatQuantity) {
-      return res.status(400).json({ error: "Missing fields" });
-    }
-    const hallSeats = [];
-    for (let i = 0; i < seatQuantity; i++) {
-      const seat = new Seat({ seatNumber: `H${hallNumber}S${ i + 1 }` });
-      await seat.save();
-      hallSeats.push(seat._id);
-    }
-    const hall = new Hall({ hallNumber, seats: hallSeats });
-    await hall.save();
-    res.status(200).json({ message: "Hall created successfully", hall });
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
-});
+hallrouter.post("/", createHall);
 
+// Get all halls
+// /api/hall
+hallrouter.get("/", getHalls);
+
+// Get a hall by id and populate the seats
+// /api/hall/:id
+hallrouter.get("/:id", getHallById);
+
+// get all showtimes of a movie in a hall
+// /api/hall/:hallId/showtimes/:movieId
+hallrouter.get("/:hallId/showtimes/:movieId", getShowtimesOfMovieInHall);
+
+// delete all halls and their seats and showtimes
+// /api/hall
+hallrouter.delete("/", deleteHalls);
+
+// get all seats of a hall at a specific showtime
+// /api/hall/:hallId/showtime/:showtime
+hallrouter.get("/:hallId/showtime/:showtime", getSeatsOfHallAtShowtime);
+
+// get a seat info
+hallrouter.get("/seat/:id", getSeatInfo)
 export default hallrouter;
