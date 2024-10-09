7d261a7a2ced07f74a7f890e54550e75be8ceb2a
Kim Andersson <kandersson@gmail.com>
2024-10-09 15:06:14 +0200

    material ui added to buttons in header

--- front-end/src/layout/Header/Header.tsx
+++ front-end/src/layout/Header/Header.tsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import './Header.css';
 import Logo from '../../components/Logo/Logo';
+import Button from '@mui/material/Button';
+import SearchIcon from '../../assets/icons/search_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png';
+import LoginIcon from '../../assets/icons/person_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png'
 
 const Header: React.FC = () => {
   return (
     <header>
       <div className="schedule-button-container">
-        <button>Idag</button>
-        <button>Imorgon</button>
-        <button>Senare</button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: '80px',
+            width: '80px',
+            border: 'none', 
+            borderRight: '2px solid #fcaf00',
+            color: '#fcaf00',
+          }}
+        >Idag</Button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: '80px',
+            width: '80px',
+            border: 'none', 
+            borderRight: '2px solid #fcaf00',
+            color: '#fcaf00',
+          }}
+        >Imorgon</Button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: '80px',
+            width: '80px',
+            border: 'none', 
+            borderRight: '2px solid #fcaf00',
+            color: '#fcaf00',
+          }}
+        >Senare</Button>
       </div>
       <div className='logo-container'>
         <Logo />
       </div>
       <div className="search-login-container">
-        <button>search</button>
-        <button>login</button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: '80px',
+            width: '80px',
+            border: 'none', 
+            borderLeft: '2px solid #fcaf00',
+            color: '#fcaf00',
+          }}
+        ><img src={SearchIcon} alt="Search" style={{ height: '40px', width: '40px' }} /></Button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: '80px',
+            width: '80px',
+            border: 'none', 
+            borderLeft: '2px solid #fcaf00',
+            color: '#fcaf00',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        ><img src={LoginIcon} alt="Search" style={{ height: '40px', width: '40px'}}/>Login</Button>
       </div>
     </header>
   );
