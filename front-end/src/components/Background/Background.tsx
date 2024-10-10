501fc49a1f84d994315b44d5d5bc6d6abe2117bc
Kim Andersson <kandersson@gmail.com>
2024-10-10 09:27:49 +0200

    added background

---
+++ front-end/src/components/Background/Background.tsx
@@ -0,0 +1,13 @@
+import './Background.css';
+
+function Background() {
+    return (
+      <div className="background">
+        <div className="background__left"></div>
+        <div className="background__middle"></div>
+        <div className="background__right"></div>
+      </div>
+    );
+}
+
+export default Background;
