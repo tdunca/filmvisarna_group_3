786b7ababfe959c97433b939553cef64dd2484d2
Kim Andersson <kandersson@gmail.com>
2024-10-16 12:56:46 +0200

    MovieComponent is working and is listed in MovieCollectionSecrion

--- front-end/src/components/MovieComponent/MovieComponent.tsx
+++ front-end/src/components/MovieComponent/MovieComponent.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MovieComponent.css';
 
-interface MovieProps {
-  movie: {
+interface MovieButtonProps {
 	_id: string;
-    title: string;
+	title: string;
+	year: number;
 	poster: string;
-	genre: [string];
-
-  };
+	genre: string[];
+	ageRestriction: number;
 }
 
-const MovieComponent: React.FC<MovieProps> = ({ movie }) => {
-  return (
-    <article>
-		<Link to={`/movie-info/${movie.id}`}>
-			<img>{movie.poster} </img>
-			<h2>{movie.title}</h2>
-			{movie.genre && <p>{movie.genre}</p>}
+const MovieComponent: React.FC<MovieButtonProps> = ({ _id, title, year, poster, genre, ageRestriction }) => {
+	return (
+		<Link to={`/movie-info/${_id}`}>
+			<article className="movie-button">
+				<img src={poster} alt={title} />
+				<h2>{title}</h2>
+				<p>{year}</p>
+				<p>{genre.join(', ')}</p>
+				<p>{ageRestriction}</p>
+			</article>
 		</Link>
-
-    </article>
-  );
+	);
 };
 
 export default MovieComponent;
