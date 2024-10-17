10f9ef3c418dd3ea2e0dcf5d6c9734689390d983
Nuur Yusuf <112573333+nyusufdot88@users.noreply.github.com>
2024-10-18 00:29:43 +0200

    Added social media and öppettidder

--- front-end/src/layout/Footer/Footer.tsx
+++ front-end/src/layout/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
         <div className="row text-light">
           <div className="col-md-3">
             <h5>Om oss</h5>
-            <ul className="footer-omoss">
+            <ul className="list-unstyled">
               <li>
                 <Link to="/about-us">Om oss</Link>
               </li>
@@ -20,6 +20,38 @@ const Footer: React.FC = () => {
                 <Link to="/about-cinemas">Våra biografer</Link>
               </li>
             </ul>
+          </div>
+
+          <div className="col md-3">
+            <h5>Öppettider</h5>
+            <ul className="list-unstyled">
+              <li>Måndag - Torsdag: 08:00 - 01:00</li>
+              <li>Fredag - Lördag: 08:00 - 03:00</li>
+              <li>Söndag: 10:00 - 01:00</li>
+            </ul>
+          </div>
+
+          <div className="col-md-3">
+            <h5>Adress:</h5>
+            <p>Smågatan 5, Småstad</p>
+          </div>
+
+          <div className="col-md-3 text-center">
+            <h5>Sociala medier</h5>
+            <div className="social-media-links">
+              <a title="facebook" href="#">
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a title="instagram" href="#">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a title="twitter" href="#">
+                <i className="bi bi-twitter"></i>
+              </a>
+              <a title="youtube" href="#">
+                <i className="bi bi-youtube"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
