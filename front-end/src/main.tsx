50fdc240947eeac872e68c04d8d6608d14627705
Nuur Yusuf <112573333+nyusufdot88@users.noreply.github.com>
2024-10-18 15:54:34 +0200

    taggit bort bootstrap-icons in main

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Background from './components/Background/Background';
